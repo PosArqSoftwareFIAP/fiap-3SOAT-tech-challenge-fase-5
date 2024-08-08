2be966340265c88e928e57572906869850f8e4d5
Leonilson Alencar <86755623+Ralrargrun@users.noreply.github.com>
2024-08-08 18:09:32 -0300

    up

---
+++ pedido-app/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+
+# variaveis de banco de dados
+db_host     = os.getenv('MYSQL_HOST')
+db_user     = os.getenv('MYSQL_USER')
+db_password = os.getenv('MYSQL_PASSWORD')
+db_port     = 25060
+db_database = 'FIAP-FOOD'
+
+
+# variaveis de...
